9c4c20e29b93f99ffda3c71e79ba6a6a35b4c872
Trey McGarity <trey.mcgarity99@gmail.com>
2020-09-11 10:26:33 -0500

    ring buffer mvp

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,25 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.storage = []
+        self.capacity = capacity
+        self.index = 0 #needed to maintain position in list
 
     def append(self, item):
-        pass
+        # if size of storage is within capacity
+        if len(self.storage) < self.capacity:
+            # append value at end of current storage
+            self.storage.append(item)
+        else:
+            # if index hits capacity (handles proper indexing)
+            if self.index == self.capacity:
+                # set index back to zero 
+                self.index = 0
+            # pop storage at index (to maintain rest of list order)
+            self.storage.pop(self.index)
+            # then insert value at index
+            self.storage.insert(self.index, item)
+            # increment index
+            self.index += 1
 
     def get(self):
-        pass
+        return self.storage
