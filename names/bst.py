1d8d2fe956d18da816ebd93e0af43fcaa3a25dd1
Trey McGarity <trey.mcgarity99@gmail.com>
2020-09-11 11:08:22 -0500

    names mvp with big O calculation

---
+++ names/bst.py
@@ -0,0 +1,31 @@
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from bst import BST
 
 start_time = time.time()
 
@@ -12,12 +13,39 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
+"""
+Replace the nested for loops below with your improvements
+efficiency of O(n^2) because: 
+    nested for loops exponential increase work 
+
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
 
+runtime = 6 seconds
+----------------------------------------------------------
+Binary Search Tree improvement
+efficienct of O(n log n) because:
+    O(log n) # Binary Search Tree
+  + O(n)     # Size of second file
+"""
+#initialize the Binary Search Tree
+bst = BST('')
+#traverse file line by line
+for name_1 in names_1:
+    #insert each line into BST
+    bst.insert(name_1)
+
+#traverse other file line by line
+for name_2 in names_2:
+    #if current line value is in BST
+    if bst.contains(name_2):
+        #add value to array of duplicates
+        duplicates.append(name_2)
+"""
+runtime = .06 seconds
+"""
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
